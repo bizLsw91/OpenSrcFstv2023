6f5c874e67dc5764b6b3860935474edd0a9123f4
bizLsw <biz.lsw91@gmail.com>
2023-11-07 04:16:43 +0900

    overView - 시간안내 1차 완료.

---
+++ src/data/ProgramData.js
@@ -0,0 +1,47 @@
+const overall = {
+    a:{
+        time:1,
+        title: '개회식 & 시상식',
+        place: '401'
+    },
+    b:{
+        time:2,
+        title: '기조강연 1부,2부',
+        place: '401'
+    },
+    b2:{
+        time:2,
+        title: '스프린트',
+        place: '403'
+    },
+    c:{
+        time:3,
+        title: '발표세션',
+        place: '401'
+    },
+    c2:{
+        time:3,
+        title: '패널토크',
+        place: '402'
+    },
+    c3:{
+        time:3,
+        title: '스프린트',
+        place: '403'
+    },
+    d:{
+        time:4,
+        title: '경품추첨 및 종료',
+        place: ''
+    },
+}
+
+
+const data401ho = [
+    {
+
+    }
+]
+
+
+export {overall}
--- src/pages/Overview/OverviewArea/OverviewArea.js
+++ src/pages/Overview/OverviewArea/OverviewArea.js
@@ -1,28 +1,147 @@
 import {Col, Container, Row} from "react-bootstrap";
 import infoData from "../../../data/OverviewData";
+
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import {overall} from "../../../data/ProgramData";
+import {Chip, Stack} from "@mui/material";
+
+
 const OverviewArea = () => {
     return (
         <div id="OverviewArea">
             <Container>
                 <div className="overview__area">
-                    <h2 className="overview__title">행사 개요</h2>
                     <div className="overview__content">
-                        <section className="overview__textInfo">
-                            <Row className="flex-column">
-                                {infoData.map((info, index) => (
-                                    <Col key={index} className="mb-20">
-                                        <td>
-                                            <div className="item">{info.item}</div>
-                                        </td>
-                                        <td>
-                                            <div className="desc">{info.desc}</div>
-                                            {info.desc_sub && <p className="desc-sub">{info.desc_sub}</p>}
-                                        </td>
-                                    </Col>
-                                ))}
-                            </Row>
-                        </section>
-                        <section className="overview__image"></section>
+                        <Row>
+                            <Col md={7}>
+                                <h3 className="overview__title">행사 개요</h3>
+                                <section className="overview__textInfo">
+                                    <Row className="flex-column">
+                                        {infoData.map((info, index) => (
+                                            <Col key={index} className="mb-20">
+                                                <td>
+                                                    <div className="item">{info.item}</div>
+                                                </td>
+                                                <td>
+                                                    <div className="desc">{info.desc}</div>
+                                                    {info.desc_sub && <p className="desc-sub">{info.desc_sub}</p>}
+                                                </td>
+                                            </Col>
+                                        ))}
+                                    </Row>
+                                </section>
+                            </Col>
+                            <Col md={5}>
+                                <section className="overview__schedule">
+                                    <h3 className="overview__title">시간 안내</h3>
+                                    <Timeline>
+                                        <TimelineItem>
+                                            <TimelineOppositeContent color="textSecondary">
+                                                1:00 pm
+                                            </TimelineOppositeContent>
+                                            <TimelineSeparator>
+                                                <TimelineDot variant="outlined"/>
+                                                <TimelineConnector/>
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <div className="contentBox">
+                                                    <div className="item">
+                                                        <div className="ho ho-color1">{overall.a.place+'호'}</div>
+                                                        <a className="title">{overall.a.title+' >'}</a>
+                                                    </div>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                        <TimelineItem>
+                                            <TimelineOppositeContent color="textSecondary">
+                                                2:00 pm
+                                            </TimelineOppositeContent>
+                                            <TimelineSeparator>
+                                                <TimelineDot variant="outlined"/>
+                                                <TimelineConnector/>
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <div className="contentBox">
+                                                    <div className="item">
+                                                        <div className="ho ho-color1">{overall.b.place+'호'}</div>
+                                                        <a className="title">{overall.b.title+' >'}</a>
+                                                    </div>
+                                                    <div className="item">
+                                                        <div className="ho ho-color3">{overall.b2.place+'호'}</div>
+                                                        <a className="title">{overall.b2.title+' >'}</a>
+                                                    </div>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                        <TimelineItem>
+                                            <TimelineOppositeContent color="textSecondary">
+                                                3:00 pm
+                                            </TimelineOppositeContent>
+                                            <TimelineSeparator>
+                                                <TimelineDot variant="outlined"/>
+                                                <TimelineConnector/>
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <div className="contentBox">
+                                                    <div className="item">
+                                                        <div className="ho ho-color1">{overall.c.place+'호'}</div>
+                                                        <a className="title">{overall.c.title+' >'}</a>
+                                                    </div>
+                                                    <div className="item">
+                                                        <div className="ho ho-color2">{overall.c2.place+'호'}</div>
+                                                        <a className="title">{overall.c2.title+' >'}</a>
+                                                    </div>
+                                                    <div className="item">
+                                                        <div className="ho ho-color3">{overall.c3.place+'호'}</div>
+                                                        <a className="title">{overall.c3.title+' >'}</a>
+                                                    </div>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                        <TimelineItem>
+                                            <TimelineOppositeContent color="textSecondary">
+                                                5:40 pm
+                                            </TimelineOppositeContent>
+                                            <TimelineSeparator>
+                                                <TimelineDot variant="outlined"/>
+                                                <TimelineConnector/>
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <div className="contentBox last">
+                                                <div className="item">
+                                                    <div className="ho ho-color3"></div>
+                                                    <a className="title">{overall.d.title}</a>
+                                                </div>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                        <TimelineItem>
+                                            <TimelineOppositeContent color="textSecondary">
+                                                6:00 pm
+                                            </TimelineOppositeContent>
+                                            <TimelineSeparator>
+                                                <TimelineDot variant="outlined"/>
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <div className="contentBox ops">
+                                                    <div className="item">
+                                                        <div className="ho ho-color3"></div>
+                                                        <a className="title">{overall.d.title}</a>
+                                                    </div>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                    </Timeline>
+                                </section>
+                            </Col>
+                        </Row>
+                        {/*<section className="overview__image"></section>*/}
                     </div>
                 </div>
             </Container>
