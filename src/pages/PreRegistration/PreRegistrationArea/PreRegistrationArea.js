9a2aee70c50780a8c059c20f449c957d2de9a752
bizLsw <biz.lsw91@gmail.com>
2023-11-07 06:52:55 +0900

    사전등록 작업중

--- src/pages/PreRegistration/PreRegistrationArea/PreRegistrationArea.js
+++ src/pages/PreRegistration/PreRegistrationArea/PreRegistrationArea.js
@@ -1,10 +1,39 @@
+import React from "react";
 import {Container} from "react-bootstrap";
+import {Box, Tab, Tabs} from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 const PreRegistrationArea = () => {
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <div id="PreRegistrationArea">
             <Container>
-                <h1 className="p-5">준비중</h1>
+                <div className="preRegi__area">
+                    <div className="preRegi__content">
+                        <Box sx={{width: '100%', typography: 'body1'}}>
+                            <TabContext value={value}>
+                                <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                        <Tab className="tabBtn" label="사전 등록" value="1"/>
+                                        <Tab className="tabBtn" label="등록 확인" value="2"/>
+                                    </TabList>
+                                </Box>
+                                <TabPanel value="1">
+
+                                </TabPanel>
+                                <TabPanel value="2">
+                                    Item Two</TabPanel>
+                            </TabContext>
+                        </Box>
+                    </div>
+                </div>
             </Container>
         </div>
     );
--- src/pages/Program/ProgramArea/ProgramArea.js
+++ src/pages/Program/ProgramArea/ProgramArea.js
@@ -1,12 +1,25 @@
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
+import {useState} from "react";
 
 const ProgramArea = () => {
+    const [currTabId, setCurrTabId] = useState(1);
+
+
+
     return (
         <div id="ProgramArea">
             <Container>
-                <div className="common__area">
-                    <div className="common__content">
-                        <h2 className="common__title">프로그램</h2>
+                <div className="program__area">
+                    <div className="program__content">
+                        <h2 className="program__title">프로그램</h2>
+                        <Row>
+                            <Col className="table-overall">
+                                <Row className="tabGroup">
+                                    <Col></Col>
+                                </Row>
+                            </Col>
+                            <Col></Col>
+                        </Row>
 
                     </div>
                 </div>
