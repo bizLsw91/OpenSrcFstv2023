6a706ed436248de5678bfb2797ea7277ce2cc8d8
dev웅 <dev.lsw91@gmail.com>
2023-11-08 14:27:17 +0900

    행사개요에 링크 추가.

--- src/pages/Overview/OverviewArea/OverviewArea.js
+++ src/pages/Overview/OverviewArea/OverviewArea.js
@@ -1,5 +1,4 @@
 import {Col, Container, Row} from "react-bootstrap";
-import infoData from "../../../data/OverviewData";
 
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
@@ -9,8 +8,19 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import {overall} from "../../../data/ProgramData";
-import {Chip, Stack} from "@mui/material";
+import {Link} from "react-router-dom";
 
+const infoData = [
+    {item: '개최목적', desc: '공개SW 최신 기술 및 우수사례 등을 공유·전파하고, 공개SW 시상을 통해 공개SW 인식 제고 도모'},
+    {item: '주제', desc: '오픈소스로 바꾸는 세상(Transforming the World with Open source)',
+        desc_sub: '- 공개SW 개발자, 커뮤니티, 기업의 공개SW 활용 경험과 역량을 개발자들과 공유하여 오픈소스 생태계의 성장과 발전 계기 마련'
+    },
+    {item: '참가자', desc: '공개SW개발자, 관련 기업, 학생 등 약 300여명'},
+    {item: '장소', desc: '코엑스 401호~03호'},
+    {item: '일시', desc: '2023.12.01.(금) 13:00~18:00'},
+    {item: '주최', desc: '과학기술정보통신부'},
+    {item: '주관', desc: '정보통신산업진흥원'},
+]
 
 const OverviewArea = () => {
     return (
@@ -23,17 +33,76 @@ const OverviewArea = () => {
                                 <h3 className="overview__title">행사 개요</h3>
                                 <section className="overview__textInfo">
                                     <Row className="flex-column">
-                                        {infoData.map((info, index) => (
-                                            <Col key={index} className="mb-20">
-                                                <td>
-                                                    <div className="item">{info.item}</div>
-                                                </td>
-                                                <td>
-                                                    <div className="desc">{info.desc}</div>
-                                                    {info.desc_sub && <p className="desc-sub">{info.desc_sub}</p>}
-                                                </td>
-                                            </Col>
-                                        ))}
+                                        <Col className="mb-20">
+                                            <td>
+                                                <div className="item">{infoData[0].item}</div>
+                                            </td>
+                                            <td>
+                                                <div className="desc">{infoData[0].desc}</div>
+                                            </td>
+                                        </Col>
+                                    </Row>
+                                    <Row className="flex-column">
+                                        <Col className="mb-20">
+                                            <td>
+                                                <div className="item">{infoData[1].item}</div>
+                                            </td>
+                                            <td>
+                                                <div className="desc">{infoData[1].desc}</div>
+                                                <p className="desc-sub">{infoData[1].desc_sub}</p>
+                                            </td>
+                                        </Col>
+                                    </Row>
+                                    <Row className="flex-column">
+                                        <Col className="mb-20">
+                                            <td>
+                                                <div className="item">{infoData[2].item}</div>
+                                            </td>
+                                            <td>
+                                                <div className="desc">{infoData[2].desc}</div>
+                                            </td>
+                                        </Col>
+                                    </Row>
+                                    <Row className="flex-column">
+                                        <Col className="mb-20 d-flex">
+                                            <td>
+                                                <div className="item">{infoData[3].item}</div>
+                                            </td>
+                                            <td className="d-flex">
+                                                <div className="desc">{infoData[3].desc}</div>
+                                                <Link className="directions ml-15" to="/directions">찾아오시는 길 ></Link>
+                                            </td>
+                                        </Col>
+                                    </Row>
+                                    <Row className="flex-column">
+                                        <Col className="mb-20">
+                                            <td>
+                                                <div className="item">{infoData[4].item}</div>
+                                            </td>
+                                            <td>
+                                                <div className="desc">{infoData[4].desc}</div>
+                                            </td>
+                                        </Col>
+                                    </Row>
+                                    <Row className="flex-column">
+                                        <Col className="mb-20">
+                                            <td>
+                                                <div className="item">{infoData[5].item}</div>
+                                            </td>
+                                            <td>
+                                                <div className="desc">{infoData[5].desc}</div>
+                                            </td>
+                                        </Col>
+                                    </Row>
+                                    <Row className="flex-column">
+                                        <Col className="mb-20">
+                                            <td>
+                                                <div className="item">{infoData[6].item}</div>
+                                            </td>
+                                            <td>
+                                                <div className="desc">{infoData[6].desc}</div>
+                                            </td>
+                                        </Col>
                                     </Row>
                                 </section>
                             </Col>
@@ -53,7 +122,7 @@ const OverviewArea = () => {
                                                 <div className="contentBox">
                                                     <div className="item">
                                                         <div className="ho ho-color1">{overall.a.place+'호'}</div>
-                                                        <a className="title">{overall.a.title+' >'}</a>
+                                                        <a className="title">{overall.a.title}</a>
                                                     </div>
                                                 </div>
                                             </TimelineContent>
@@ -70,11 +139,11 @@ const OverviewArea = () => {
                                                 <div className="contentBox">
                                                     <div className="item">
                                                         <div className="ho ho-color1">{overall.b.place+'호'}</div>
-                                                        <a className="title">{overall.b.title+' >'}</a>
+                                                        <a className="title">{overall.b.title}</a>
                                                     </div>
                                                     <div className="item">
                                                         <div className="ho ho-color3">{overall.b2.place+'호'}</div>
-                                                        <a className="title">{overall.b2.title+' >'}</a>
+                                                        <a className="title">{overall.b2.title}</a>
                                                     </div>
                                                 </div>
                                             </TimelineContent>
@@ -91,15 +160,15 @@ const OverviewArea = () => {
                                                 <div className="contentBox">
                                                     <div className="item">
                                                         <div className="ho ho-color1">{overall.c.place+'호'}</div>
-                                                        <a className="title">{overall.c.title+' >'}</a>
+                                                        <a className="title">{overall.c.title}</a>
                                                     </div>
                                                     <div className="item">
                                                         <div className="ho ho-color2">{overall.c2.place+'호'}</div>
-                                                        <a className="title">{overall.c2.title+' >'}</a>
+                                                        <a className="title">{overall.c2.title}</a>
                                                     </div>
                                                     <div className="item">
                                                         <div className="ho ho-color3">{overall.c3.place+'호'}</div>
-                                                        <a className="title">{overall.c3.title+' >'}</a>
+                                                        <a className="title">{overall.c3.title}</a>
                                                     </div>
                                                 </div>
                                             </TimelineContent>
