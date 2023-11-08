0307eaececf9419eb5a3b172b4314b9462687f00
dev웅 <dev.lsw91@gmail.com>
2023-11-08 13:59:24 +0900

    이벤트 페이지 테스트

--- src/pages/Event/EventArea/EventArea.js
+++ src/pages/Event/EventArea/EventArea.js
@@ -1,10 +1,23 @@
 import {Container} from "react-bootstrap";
+import {Image} from "antd";
 
 const EventArea = () => {
     return (
         <div id="EventArea">
             <Container>
-                <h1 className="p-5">준비중</h1>
+                <div className="program__area">
+                    <div className="program__content">
+                        <h2 className="program__title">이벤트</h2>
+                        <p>※클릭하시면 확대해서 보실 수 있습니다.</p>
+                        <Image
+                            className="imgBox"
+                            movable={true}
+                            src='assets/img/edm/edm-event.png' // 이미지 URL을 넣으세요
+                            preview={{ src: "assets/img/edm/edm-event.png" }} // 클릭할 때만 확대되도록 설정
+                        />
+
+                    </div>
+                </div>
             </Container>
         </div>
     );
