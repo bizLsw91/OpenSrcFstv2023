e99ec6e59ec85ef83bc1a3996c51303f5c09320e
bizLsw <biz.lsw91@gmail.com>
2023-11-10 23:10:33 +0900

    git CLI 테스트

--- src/pages/Overview/OverviewArea/OverviewArea.js
+++ src/pages/Overview/OverviewArea/OverviewArea.js
@@ -12,7 +12,7 @@ import {Link} from "react-router-dom";
 
 const infoData = [
     {item: '개최목적', desc: '공개SW 최신 기술 및 우수사례 등을 공유·전파하고, 공개SW 시상을 통해 공개SW 인식 제고 도모'},
-    {item: '주제', desc: '오픈소스로 바꾸는 세상 (Transforming the World with Open source)',
+    {item: '주제', desc: '오픈소스로 바꾸는 세상(Transforming the World with Open source)',
         desc_sub: '- 공개SW 개발자, 커뮤니티, 기업의 공개SW 활용 경험과 역량을 개발자들과 공유하여 오픈소스 생태계의 성장과 발전 계기 마련'
     },
     {item: '참가자', desc: '공개SW개발자, 관련 기업, 학생 등 약 300여명'},
