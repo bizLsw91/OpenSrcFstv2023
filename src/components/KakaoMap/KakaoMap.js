2bd9bca94e25a33e07c2c2dbbf06b7ca714601d4
bizLsw <biz.lsw91@gmail.com>
2023-11-10 23:01:00 +0900

    오시는길 수정

--- src/components/KakaoMap/KakaoMap.js
+++ src/components/KakaoMap/KakaoMap.js
@@ -4,14 +4,8 @@ import {useEffect} from "react";
 
 const {kakao} = window;
 
-// const content = ()=>{return (
-//     <div className="overlay">여기는 코엑스 컨퍼런스룸입니다!</div>
-// ) }; // 오버레이에 표시할 내용
 
-const content =
-    '<div class="infoWindowBox">코엑스 컨퍼런룸 4F<br/>' +
-    '<a href="https://place.map.kakao.com/m/17573702" class="underline__blue">코엑스 상세교통편</a><br/>' +
-    '<a href="https://www.coex.co.kr/blog/coex-facility-info/conference_south_4f" class="underline__blue">회의실 시설안내</a></div>'; // 오버레이에 표시할 내용
+const content = '<div class="mapInfoWindowBox">코엑스 컨퍼런룸 4F</div>'
 
 const KakaoMap = () => {
     // 마커를 표시할 위치 설정
