a0c61b9842561d461c3575c64989a46cb13494f4
bizLsw <biz.lsw91@gmail.com>
2023-11-10 14:30:19 +0900

    antd Image error fix

--- src/pages/Event/EventArea/EventArea.js
+++ src/pages/Event/EventArea/EventArea.js
@@ -11,7 +11,6 @@ const EventArea = () => {
                         <p className="d-sm-none">※클릭하시면 확대해서 보실 수 있습니다.</p>
                         <Image
                             className="imgBox"
-                            movable={true}
                             src='assets/img/edm/edm-event.png' // 이미지 URL을 넣으세요
                             preview={{ src: "assets/img/edm/edm-event.png" }} // 클릭할 때만 확대되도록 설정
                         />
--- src/pages/Home/HomeArea/HomeArea.js
+++ src/pages/Home/HomeArea/HomeArea.js
@@ -10,7 +10,6 @@ const HomeArea = () => {
                     <div className="home__content">
                         <Image
                             maskClassName="imgBox"
-                            movable={true}
                             src='assets/img/openSW/openSW-program.png' // 이미지 URL을 넣으세요
                             preview={{src: "assets/img/openSW/openSW-program.png"}} // 클릭할 때만 확대되도록 설정
                         />
