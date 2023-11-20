566f581d55de9346d5836c2b014cdff74578054a
bizLsw <biz.lsw91@gmail.com>
2023-11-20 22:58:10 +0900

    robots noindex -> index

--- src/components/shared/PageHelmet.js
+++ src/components/shared/PageHelmet.js
@@ -7,8 +7,8 @@ const PageHelmet = ({ pageTitle }) => {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>{pageTitle ? pageTitle+' || ' : ''}공개SW 페스티벌 2023 </title>
-                <meta name="robots" content="noindex, follow" />
-                <meta name="description" content="open_src_fstv2023 - Consulting React Template" />
+                <meta name="robots" content="index, follow" />
+                <meta name="description" content="오픈소스로 바꾸는 세상 OpenSource 2023 공개SW페스티벌" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             </Helmet>
         </>
