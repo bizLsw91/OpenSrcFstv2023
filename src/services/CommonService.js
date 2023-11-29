29f36fe9b6bb94d91f6fd0dd1f9017015b9b7815
bizLsw <biz.lsw91@gmail.com>
2023-11-30 05:08:04 +0900

    발표자료 열람 기능 추가

--- src/services/CommonService.js
+++ src/services/CommonService.js
@@ -7,3 +7,6 @@ req ex) {collectionPath: isSprint?'UserSpr':'User', error:{stack:err.stack}, pay
 export const api_addErrLog = async (req) => {
     return await axios.post(appConfig.apiPreUrl + '/Common/Error/addErrLog',req)
 };
+export const api_download = async (req, config) => {
+    return await axios.post(appConfig.apiPreUrl + '/Common/download',req, config)
+};
