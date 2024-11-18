9e7d9d351c8bbeca5141a473db660b0605a45529
bizLsw91 <biz.lsw91@gmail.com>
2024-11-19 04:59:08 +0900

    key-visual 배경 layer 3단계 적용

--- src/pages/Home/HomeKeyVisual/HomeKeyVisual.js
+++ src/pages/Home/HomeKeyVisual/HomeKeyVisual.js
@@ -1,34 +1,38 @@
 import React, {useContext} from 'react';
 import Slider from 'react-slick';
-import { Link } from 'react-router-dom';
-import { Col, Container, Row } from "react-bootstrap";
+import {Link} from 'react-router-dom';
+import {Col, Container, Row} from "react-bootstrap";
 import appConfig from "../../../config/app.config";
 import {AppContext} from "../../../context/AllContext";
 
 const isShow = appConfig.isShow
 const HomeKeyVisual = () => {
     const {isOverDeadLine} = useContext(AppContext);
-   return (
-      <>
-         <section className="home_slider_1">
-               <Container className="h1_slider_wrapper h-100">
-                        <div className="slider__content-6">
-                           <img className="logo-top" src="assets/img/banner/top.png" alt=""/>
-                           <img className="logo-keyvisual" src="assets/img/banner/keyvisual_desktop_text.png" alt=""/>
-                           <div className="slider__btn mainBannerBtn">
-                              {!isOverDeadLine() &&
-                                  <div className="preRegistration">
+    return (
+        <>
+            <section className="key-visual">
+                <div className={'key-visual-bg_layer'}>
+                    <div className="layer-1"></div>
+                    <div className="layer-2"></div>
+                </div>
+                <Container className="h1_slider_wrapper h-100">
+                    <div className="slider__content-6">
+                        <img className="logo-top" src="assets/img/banner/top.png" alt=""/>
+                        <img className="logo-keyvisual" src="assets/img/banner/keyvisual_desktop_text.png" alt=""/>
+                        <div className="slider__btn mainBannerBtn">
+                            {!isOverDeadLine() &&
+                                <div className="preRegistration">
                                     <Link to="/preRegistration" className="z-btn-main z-btn-transparent">사전등록</Link>
                                     <div className="info">등록마감: 11. 30.(금) 10:00 까지</div>
-                                  </div>
-                              }
-                           </div>
-                           <img className="logo-bottom" src="assets/img/banner/host.png" alt=""/>
+                                </div>
+                            }
                         </div>
-               </Container>
-         </section>
-      </>
-   );
+                        <img className="logo-bottom" src="assets/img/banner/host.png" alt=""/>
+                    </div>
+                </Container>
+            </section>
+        </>
+    );
 };
 
 export default HomeKeyVisual;
