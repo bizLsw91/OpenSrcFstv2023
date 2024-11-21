693137a2bd8c9170477ea165edcb7c119b1ab4df
bizLsw91 <biz.lsw91@gmail.com>
2024-11-21 20:01:30 +0900

    사전등록버튼 무조건 활성화

--- src/pages/Home/HomeKeyVisual/HomeKeyVisual.js
+++ src/pages/Home/HomeKeyVisual/HomeKeyVisual.js
@@ -38,18 +38,18 @@ const HomeKeyVisual = () => {
                             </div>
                             <img className="mid1" src="assets/img/2024design/keyvisual-mid1.png" alt="keyvisual-mid1"/>
                             <div className="slider__btn mainBannerBtn">
-                                {!isOver ?
-                                    <div className="preRegistration">
-                                        <Link to="/"
-                                              className={`z-btn-main z-btn-transparent z-btn-disable`}>사전등록</Link>
-                                        <div className="info">2024.11.20 (수) 12:00 부터 신청가능</div>
-                                    </div> :
+                                {/*{!isOver ?*/}
+                                {/*    // <div className="preRegistration">*/}
+                                {/*    //     <Link to="/"*/}
+                                {/*    //           className={`z-btn-main z-btn-transparent z-btn-disable`}>사전등록</Link>*/}
+                                {/*    //     <div className="info">2024.11.20 (수) 12:00 부터 신청가능</div>*/}
+                                {/*    // </div> :*/}
                                     <div className="preRegistration">
                                         <Link to="https://www.onoffmix.com/event/313420"
                                               className={`z-btn-main z-btn-transparent`}>사전등록</Link>
                                         <div className="info">2024.12.6 (금) 12:00 까지 선착순 정원 500명 신청가능</div>
                                     </div>
-                                }
+                                {/*}*/}
                             </div>
                             <img className="mid2" src="assets/img/2024design/keyvisual-mid2.png" alt="keyvisual-mid2"/>
                         </div>
